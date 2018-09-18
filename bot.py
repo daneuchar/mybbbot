4aeb5423fb36fb1073e5c8d4efd8f738df6856b9
daneuchar <daneuchar@gmail.com>
2018-09-18 18:33:10 +0530

    MybbBot

---
+++ bot.py
@@ -0,0 +1,79 @@
+import requests
+import discord
+from discord.ext import commands
+
+TOKEN = 'NDg5MDY5NzE1NDAzMzA5MDY2.DnlbSQ.WDw-HSh9rm9QolC9_lwhUYg2Y3k'
+
+client = commands.Bot(command_prefix = '.')
+
+
+def forumPost(data , section, user):
+    delm = 0
+    title = ''
+    content = ''
+    for word in data:
+        if delm == 0 and word != ';':
+            title += word
+            title += ' '
+        elif delm == 1:
+            content += word
+            content += " "
+        if word == ';':
+            delm = 1
+    
+    content += " **- Submitted By "
+    content += user
+    content += "**"
+
+    if title != '' and content != '':
+        userdata = {"title": title, "message": content, "section": section}
+        resp = requests.post('https://imperials.io/api/thread.php', params=userdata)
+        return True
+    else:
+        return False
+
+@client.event
+async def on_ready():
+    print('Bot initiated')
+
+
+@client.command(pass_context=True)
+async def method(ctx, *args):
+    author = str(ctx.message.author)
+    status=forumPost(args, 21, author)
+    if status:
+        await client.say("The title has been posted Successfully")
+    else:
+        await client.say("Wrong Format kindly use this format: .SectionName Thread Title ; Thread Content")
+
+
+@client.command(pass_context=True)
+async def method1(ctx, *args):
+    author = str(ctx.message.author)
+    status = forumPost(args, 23, author)
+    if status:
+        await client.say("The title has been posted Successfully")
+    else:
+        await client.say("Wrong Format kindly use this format: .SectionName Thread Title ; Thread Content")
+
+
+@client.command(pass_context=True)
+async def method2(ctx, *args):
+    author = str(ctx.message.author)
+    status = forumPost(args, 24, author)
+    if status:
+        await client.say("The title has been posted Successfully")
+    else:
+        await client.say("Wrong Format kindly use this format: .SectionName Thread Title ; Thread Content")
+
+
+@client.command(pass_context=True)
+async def method3(ctx, *args):
+    author = str(ctx.message.author)
+    status = forumPost(args, 25, author)
+    if status:
+        await client.say("The title has been posted Successfully")
+    else:
+        await client.say("Wrong Format kindly use this format: .SectionName Thread Title ; Thread Content")
+
+client.run(TOKEN)
